fd8a668579f7e202c029a53693e480e15b851ddc
Anas Ansari <anas_ansar1@outlook.com>
2021-04-27 17:05:48 +0100

    finished

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Hello World")
+let userAge = prompt("How old are you?")
+let userTravelDistance = prompt("How far will you be travelling? (in kilometers please)")
+
+let pricePerTravelledKilometer = .21
+
+let travellingPrice
+
+if (userAge < 18) {
+    travellingPrice = 0.8*(userTravelDistance * pricePerTravelledKilometer)
+} if (userAge > 65) {
+    travellingPrice = 0.6*(userTravelDistance * pricePerTravelledKilometer)
+} else {
+    travellingPrice = userTravelDistance * pricePerTravelledKilometer
+}
+
+console.log(`The cost of your fare is ` + travellingPrice + ` pounds`)
